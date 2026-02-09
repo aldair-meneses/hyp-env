9d124472e2f1716a5b1f062476fb4c49fca684ed
Aldair Meneses <aldair.meneses@protonmail.com>
2026-02-09 10:53:51 -0300

    update waybar wifi tui to use gazelle

--- setup_network.sh
+++ setup_network.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm iwd systemd-networkd systemd-resolved
+sudo pacman -Syu --noconfirm networkmanager
 
-sudo systemctl enable --now iwd
-sudo systemctl enable --now systemd-networkd
-sudo systemctl enable --now systemd-resolved
+sudo systemctl enable --now NetworkManager
 
-sudo tee /etc/systemd/network/25-wlan.network > /dev/null <<EOF
+yay -S gazelle --noconfirm --answerdiff None --answerclean None
+
+sudo tee /etc/systemd/network/25-wlan.network >/dev/null <<EOF
 [Match]
 Name=wlan0
 
 [Network]
 DHCP=yes
 EOF
-
-sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
-
-sudo systemctl restart systemd-networkd
-sudo systemctl restart iwd
-
-echo "Status da interface:"
-networkctl status wlan0
-echo "Status do DNS:"
-resolvectl status
-
